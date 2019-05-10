5283b7fe92d898d3fe1847ed93a786897f962105
bbbingo1 <846790344@qq.com>
2019-05-10 15:28:44 +0800

    基于校内的微信闲置平台小程序

---
+++ pages/my/my.js
@@ -0,0 +1,51 @@
+import { getStorageSync, setStorageSync } from '../../api/wechat'
+let app = getApp()
+
+Page({
+
+  data: {
+    userInfo: {},
+    email: 'cy920820@gmail.com',
+    version: 'v1.0.0',
+    libVersion: 'v2.6.2'
+  },
+
+  onLoad: function (options) {
+    this.setData({
+      background_color: app.globalData.globalBGColor,
+      bgRed: app.globalData.bgRed,
+      bgGreen: app.globalData.bgGreen,
+      bgBlue: app.globalData.bgBlue
+    })
+    let userInfo = getStorageSync('userInfo')
+    if (userInfo.nickName) {
+      this.setData({
+        userInfo
+      })
+    } else {
+      // 查看是否授权
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.getUserInfo({
+              success: res => {
+                this.setData({
+                  userInfo: res.userInfo
+                })
+              }
+            })
+          }
+        }
+      })
+    }
+  },
+
+  onGotUserInfo(e) {
+    let userInfo = e.detail.userInfo
+    setStorageSync('userInfo', userInfo)
+    this.setData({
+      userInfo
+    })
+  }
+})
