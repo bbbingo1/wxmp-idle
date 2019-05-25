64cbaaeaba611ecb20c21d25a67d03f3b65f9db1
滨边家的小美波 <3096237774@qq.com>
2019-05-25 19:50:04 +0800

    修改list

---
+++ components/List/List.js
@@ -0,0 +1,59 @@
+Page({
+    data: {
+        StatusBar: app.globalData.StatusBar,
+        CustomBar: app.globalData.CustomBar,
+        iconList: [{
+          icon: 'cardboardfill',
+          color: 'red',
+          badge: 120,
+          name: 'VR'
+        }, {
+          icon: 'recordfill',
+          color: 'orange',
+          badge: 1,
+          name: '录像'
+        }, {
+          icon: 'picfill',
+          color: 'yellow',
+          badge: 0,
+          name: '图像'
+        }, {
+          icon: 'noticefill',
+          color: 'olive',
+          badge: 22,
+          name: '通知'
+        }, {
+          icon: 'upstagefill',
+          color: 'cyan',
+          badge: 0,
+          name: '排行榜'
+        }, {
+          icon: 'clothesfill',
+          color: 'blue',
+          badge: 0,
+          name: '皮肤'
+        }, {
+          icon: 'discoverfill',
+          color: 'purple',
+          badge: 0,
+          name: '发现'
+        }, {
+          icon: 'questionfill',
+          color: 'mauve',
+          badge: 0,
+          name: '帮助'
+        }, {
+          icon: 'commandfill',
+          color: 'purple',
+          badge: 0,
+          name: '问答'
+        }, {
+          icon: 'brandfill',
+          color: 'mauve',
+          badge: 0,
+          name: '版权'
+        }],
+        gridCol:3,
+        skin: false
+      },
+})
