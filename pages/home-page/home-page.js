5dbdc9b4ff8402e375f609385b656ddfba21af12
滨边家的小美波 <3096237774@qq.com>
2019-05-25 22:39:17 +0800

    没图片看不出瀑布流效果诶

--- pages/home-page/home-page.js
+++ pages/home-page/home-page.js
@@ -5,6 +5,79 @@ Page({
      * 页面的初始数据
      */
     data: {
+        brick_option:{
+            icon:{
+                fill:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558804821537&di=8ee6235288fcda4f283e7c00b6559afd&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fcf3f35b88beb7236f6fcdb6a9c2f0729a0a1b4313911-ie3tVL_fw658',
+                default:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558804716549&di=bb9510e646cd11277fc8cc4f57270af8&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic2%2Fcover%2F00%2F50%2F28%2F58164e4b6b130_610.jpg'
+              }
+        },
+        dataSet:[
+            {
+                id: '1',
+                content:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                time: 1533106010,
+                likedCount: 0,
+                liked: false,
+                user: {
+                  avatar: 'user_avatar_url',
+                  username: 'Minya Chan',
+                  userId: '1'
+                },
+                images: [
+                   'pic_url', 'pic_url', 'pic_url'
+                 ]
+              },
+              {
+                id: '2',
+                content:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                backgroundColor: '#AF7AC5',
+                time: 1533106010,
+                likedCount: 0,
+                liked: false,
+                user: {
+                  avatar: 'user_avatar_url',
+                  username: 'Minya Chan',
+                  userId: '1'
+                },
+                images: [
+                   'pic_url', 'pic_url'
+                 ]
+              },
+              {
+                id: '1',
+                content:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                time: 1533106010,
+                likedCount: 0,
+                liked: false,
+                user: {
+                  avatar: 'user_avatar_url',
+                  username: 'Minya Chan',
+                  userId: '1'
+                },
+                images: [
+                   'pic_url', 'pic_url', 'pic_url'
+                 ]
+              },
+              {
+                id: '1',
+                content:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                time: 1533106010,
+                likedCount: 0,
+                liked: false,
+                user: {
+                  avatar: 'user_avatar_url',
+                  username: 'Minya Chan',
+                  userId: '1'
+                },
+                images: [
+                   'pic_url', 'pic_url', 'pic_url'
+                 ]
+              }
+            ]
     },
   
     /**
